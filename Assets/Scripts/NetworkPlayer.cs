900ea6e4e32a597aa9b2264536e352a7ec9535e7
hwookim <gusdn9754@gmail.com>
2022-05-02 16:22:20 +0900

    feat: Player Prefab 적용

--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -8,7 +8,6 @@ using Unity.XR.CoreUtils;
 public class NetworkPlayer : MonoBehaviour
 {
 
-    public Transform head;
     public Transform leftHand;
     public Transform rightHand;
 
@@ -17,19 +16,14 @@ public class NetworkPlayer : MonoBehaviour
 
     private PhotonView photonView;
 
-    private Transform headRig;
     private Transform leftHandRig;
     private Transform rightHandRig;
 
-    // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
-
-        XROrigin rig = FindObjectOfType<XROrigin>();
-        headRig = rig.transform.Find("Camera Offset/Main Camera");
-        leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
-        rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+        leftHandRig = GameObject.Find("Player").transform.Find("Camera Offset/LeftHand Controller");
+        rightHandRig = GameObject.Find("Player").transform.Find("Camera Offset/RightHand Controller");
 
         if (photonView.IsMine)
         {
@@ -40,12 +34,10 @@ public class NetworkPlayer : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (photonView.IsMine)
         {
-            MapPosition(head, headRig);
             MapPosition(leftHand, leftHandRig);
             MapPosition(rightHand, rightHandRig);
 
