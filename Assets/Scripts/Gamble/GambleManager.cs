cd330f7e1c54766a0e7384065ecf37c4465d9974
hwookim <gusdn9754@gmail.com>
2022-05-24 03:22:15 +0900

    refactor: 코인 삭제 한 번만 실행하기

--- Assets/Scripts/Gamble/GambleManager.cs
+++ Assets/Scripts/Gamble/GambleManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using Photon.Pun;
 
 public class GambleManager : MonoBehaviour
@@ -30,7 +31,6 @@ public class GambleManager : MonoBehaviour
     public static int potCoins { get; set; } = 0;
     public static int chestCoins { get; set; }
     public static float leftTime { get; set; }
-    float coinTime = 0f;
 
     void Update()
     {
@@ -54,14 +54,19 @@ public class GambleManager : MonoBehaviour
                 break;
             case State.apply:
                 OnApply();
-                coinTime = 0;
+                StartCoroutine(RemoveCoins());
                 break;
             case State.end:
             case State.loading:
                 break;
         }
-        coinTime += Time.deltaTime;
-        if (coinTime > 5f) localPlayer.RemoveCoins();
+    }
+
+    private IEnumerator RemoveCoins()
+    {
+        yield return new WaitForSeconds(5);
+        localPlayer.RemoveCoins();
+        yield break;
     }
 
     private void OnInitial()
