e0f7b90db39a59cefdcfe161e8f6c79cae20d61c
hwookim <gusdn9754@gmail.com>
2022-05-31 00:11:13 +0900

    refactor: 불필요해진 스크립트 삭제

--- Assets/Scripts/Common/XRGrabNetworkInteractable.cs
+++ Assets/Scripts/Common/XRGrabNetworkInteractable.cs
@@ -20,20 +20,12 @@ public class XRGrabNetworkInteractable : XRGrabInteractable
     protected override void OnSelectEntered(XRBaseInteractor interactor)
     {
         photonView.RequestOwnership();
-        photonView.RPC("SetUseGravity", RpcTarget.All, false);
         base.OnSelectEntered(interactor);
     }
 
     [System.Obsolete]
     protected override void OnSelectExiting(XRBaseInteractor interactor)
     {
-        photonView.RPC("SetUseGravity", RpcTarget.All, true);
         base.OnSelectExiting(interactor);
-    }
-
-    [PunRPC]
-    protected void SetUseGravity(bool value)
-    {
-        rig.useGravity = value;
     }
 }
