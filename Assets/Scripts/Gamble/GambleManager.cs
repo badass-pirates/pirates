96d42fa1724e97a95dd875c7419cdbaee3e63966
GyeongyeonAngit config --global user.name GyeongyeonAn <ruddus7444@naver.com>
2022-06-14 21:07:41 +0900

    wip

--- Assets/Scripts/Gamble/GambleManager.cs
+++ Assets/Scripts/Gamble/GambleManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using Photon.Pun;
-using static Utils;
+using UnityEngine.SceneManagement;
 
 public class GambleManager : MonoBehaviour
 {
@@ -265,6 +265,7 @@ public class GambleManager : MonoBehaviour
 
     public void MoveToReadyScene()
     {
-        StartCoroutine(U.ChangeScene("ReadyScene", 1));
+        Destroy(GameObject.Find("Voice Manager"));
+        SceneManager.LoadScene("ReadyScene", LoadSceneMode.Single);
     }
 }
