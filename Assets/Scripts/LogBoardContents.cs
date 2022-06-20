99afe7babdd1f74b27514337dec2bb6856902bf9
GyeongyeonAngit config --global user.name GyeongyeonAn <ruddus7444@naver.com>
2022-06-20 15:28:07 +0900

    bugfix: content 크기 문제 해결

--- Assets/Scripts/LogBoardContents.cs
+++ Assets/Scripts/LogBoardContents.cs
@@ -6,22 +6,9 @@ public class LogBoardContents : MonoBehaviour
 {
     public GameObject logMessage;
 
-    private List<GameObject> messageList = new List<GameObject>();
-    private Vector3 defaultScale;
-
     public void LogMessage(string message)
     {
         GameObject messageObject = Instantiate(logMessage);
-        messageList.Add(messageObject);
-        if (messageList.Count > 8)
-        {
-            defaultScale = messageObject.transform.lossyScale;
-            transform.localScale += new Vector3(0, 0.1f, 0);
-            foreach (GameObject msg in messageList)
-            {
-                msg.transform.localScale = defaultScale;
-            }
-        }
         messageObject.GetComponent<LogMessageInfo>().SetTextUI(message);
         messageObject.transform.SetParent(transform, false);
     }
