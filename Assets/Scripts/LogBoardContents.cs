6e62a03613e16d2174d1f55ace0ac8154844a6c8
GyeongyeonAngit config --global user.name GyeongyeonAn <ruddus7444@naver.com>
2022-06-20 11:12:19 +0900

    bugfix: content 늘리기 시도1

--- Assets/Scripts/LogBoardContents.cs
+++ Assets/Scripts/LogBoardContents.cs
@@ -6,10 +6,21 @@ public class LogBoardContents : MonoBehaviour
 {
     public GameObject logMessage;
 
+    private List<GameObject> messageList;
+    private Vector3 defaultScale;
+
     public void LogMessage(string message)
     {
-        GameObject q = Instantiate(logMessage);
-        q.GetComponent<LogMessageInfo>().SetTextUI(message);
-        q.transform.SetParent(transform, false);
+        GameObject messageObject = Instantiate(logMessage);
+        messageList.Add(messageObject);
+        if (messageList.Count > 8)
+        {
+            defaultScale = messageObject.transform.localScale;
+            transform.localScale += new Vector3(0, 0.1f, 0);
+            messageObject.transform.localScale = defaultScale;
+
+        }
+        messageObject.GetComponent<LogMessageInfo>().SetTextUI(message);
+        messageObject.transform.SetParent(transform, false);
     }
 }
